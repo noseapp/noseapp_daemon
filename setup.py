040a34474b9c33b91cb652b84598ccf4157dd1be
Mikhail Trifonov <mikhail.trifonov@corp.mail.ru>
2015-02-19 22:26:12 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,21 @@ from setuptools import find_packages
 import noseapp_daemon
 
 
+with open('requirements.txt') as fp:
+    requirements = [req.strip() for req in fp.readlines() if not req.startswith('--')]
+
+
 if __name__ == '__main__':
     setup(
         name='noseapp_daemon',
+        url='https://github.com/trifonovmixail/noseapp_daemon',
         version=noseapp_daemon.__version__,
         packages=find_packages(),
         author='Mikhail Trifonov',
         author_email='mikhail.trifonov@corp.mail.ru',
         description='daemon extension for noseapp lib',
-        install_requires=[
-            'psutil==2.2.1',
-        ],
+        include_package_data=True,
+        zip_safe=False,
+        platforms='any',
+        install_requires=requirements,
     )
