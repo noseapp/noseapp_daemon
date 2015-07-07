588b010698bf0b67a58ff637fd20bd90a0f2a3c1
Mikhail Trifonov <mikhail.trifonov@corp.mail.ru>
2015-07-07 18:04:16 +0300

    small fixes

--- noseapp_daemon/__init__.py
+++ noseapp_daemon/__init__.py
@@ -2,14 +2,14 @@
 
 from noseapp.datastructures import ModifyDict as Options
 
+from noseapp_daemon.runner import DaemonRunner
 from noseapp_daemon.service import DaemonService
 from noseapp_daemon.management import DaemonManagement
-from noseapp_daemon.runner import DaemonRunner as Daemon
 
 
 __all__ = (
-    Daemon,
     Options,
+    DaemonRunner,
     DaemonService,
     DaemonManagement,
 )
